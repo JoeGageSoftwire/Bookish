7495ba9fb8db10571a71ef7068c27970f1ee1386
Joseph Gage <Joseph.Gage@softwire.com>
2018-11-14 17:01:35 +0000

    Prints list of books in console

---
+++ Bookish/Bookish.ConsoleApp/Book.cs
@@ -0,0 +1,10 @@
+namespace Bookish.ConsoleApp
+{
+    class Book
+    {
+        public int Id { get; set; }
+        public string AuthorName { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+    }
+}
--- Bookish/Bookish.ConsoleApp/Program.cs
+++ Bookish/Bookish.ConsoleApp/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
 
 namespace Bookish.ConsoleApp
 {
@@ -6,7 +14,23 @@ namespace Bookish.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            string SqlString = "SELECT TOP 100 [Id],[AuthorName],[Title],[ISBN] FROM [Books]";
+
+            var ourBooks = (List<Book>) db.Query<Book>(SqlString);
+
+            foreach (var book in ourBooks)
+            {
+                Console.WriteLine(new string('*', 20));
+                Console.WriteLine("\nBook ID: " + book.Id.ToString());
+                Console.WriteLine("Author Name: " + book.AuthorName);
+                Console.WriteLine("Title: " + book.Title);
+                Console.WriteLine("ISBN: " + book.ISBN + "\n");
+                Console.WriteLine(new string('*', 20));
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
